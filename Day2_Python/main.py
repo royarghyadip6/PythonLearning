a8762117f769440998646f564dfd46885836b1b5
ARGHYADIP ROY <royarghyadip6@gmail.com>
2025-12-19 19:04:24 +0530

    Uploading Day1, Day2, Day3, Day4 Study Materials

--- Day2_Python/main.py
+++ Day2_Python/main.py
@@ -14,3 +14,9 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+# Study PEP 8 guidelines at:
+# https://peps.python.org/pep-0008/
+# or open the below file as html in your browser:
+# PEP 8 -- Style Guide for Python Code
