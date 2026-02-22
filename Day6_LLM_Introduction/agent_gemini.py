d1ee7867302460478ee225f1d7d087b058b658a5
ARGHYADIP ROY <royarghyadip6@gmail.com>
2026-02-23 03:17:55 +0530

    uploading files

--- Day6_LLM_Introduction/agent_gemini.py
+++ Day6_LLM_Introduction/agent_gemini.py
@@ -12,6 +12,7 @@ api=os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=api)
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="what is the daily Gemini API call limit?",
+    model="gemini-2.5-flash",
+    contents="what is the daily Gemini API call limit?",
 )
 print(response.text)
